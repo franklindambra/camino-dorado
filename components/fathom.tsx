f568865aace785abd2d743141c68cdd2f7004c0c
Franklin Dambra <franklindambra@gmail.com>
2026-01-27 11:03:32 -0600

    env var fix

--- components/fathom.tsx
+++ components/fathom.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import { load, trackPageview } from 'fathom-client';
-import { useEffect, Suspense } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { load, trackPageview } from "fathom-client";
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 function TrackPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    load(process.env.NEXT_PUBLIC_FATHOM_ID, {
-      auto: false
+    load(process.env.NEXT_PUBLIC_FATHOM_ID!, {
+      auto: false,
     });
   }, []);
 
@@ -19,7 +19,7 @@ function TrackPageView() {
 
     trackPageview({
       url: pathname + searchParams?.toString(),
-      referrer: document.referrer
+      referrer: document.referrer,
     });
   }, [pathname, searchParams]);
 
